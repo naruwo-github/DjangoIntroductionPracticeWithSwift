d294a9696bc9e53dac4ef62050925533c3938594
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2020-08-19 20:07:59 +0900

    URLからJSONデータ取得（バグあり）
    一部表示が不適切になっている（SwiftyJSONを使うべき）

--- DjangoPracticeApp/DjangoPracticeApp/ViewController.swift
+++ DjangoPracticeApp/DjangoPracticeApp/ViewController.swift
@@ -12,9 +12,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let url: URL = URL(string: "http://127.0.0.1:8000/api/v1/books/")!
+        let task: URLSessionTask = URLSession.shared.dataTask(with: url, completionHandler: {(data, response, error) in
+            do {
+                let jsonData = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.allowFragments)
+                print(jsonData)
+            }
+            catch {
+                print(error)
+            }
+//            print("data: \(String(describing: data))")
+//            print("response: \(String(describing: response))")
+//            print("error: \(String(describing: error))")
+        })
+        task.resume()
     }
-
 
 }
 
